c3e0865c4d111ae5000b90775fa4e963bee69f2f
Mike Bell <bell@teralogics.com>
2016-06-04 18:06:09 -0400

    a

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ process.on('uncaughtException', function (err) {
 // Set up the various directory constants.
 global.__basedir = path.join(__dirname, '..');
 global.__appdir = __dirname;
-global.__assetsdir = path.join(process.env.PWD, 'public');
+global.__assetsdir = path.join(process.env.PWD, 'app', 'public');
 global.__configdir = path.join(global.__appdir, 'config');
 global.__routesdir = path.join(global.__appdir, 'routes');
 global.__modelsdir = path.join(global.__appdir, 'models');
